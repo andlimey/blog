aa4eb6b440e1ac483cedb625eb93fd14753240fc
andlimey <andychan1451@gmail.com>
2024-05-12 11:53:03 +0800

    Add introduction

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,25 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
 
 export default function Page() {
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+    <section className="flex flex-col gap-8">
+      <h1 className="text-2xl font-semibold tracking-tighter">
+        My Personal Blog
       </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
-      </p>
-      <div className="my-8">
+      <div>
+        <p>
+          I graduated from NUS with a Bachelor's degree in computer science. I'm
+          currently working as a software engineer in Ascenda Loyalty.
+        </p>
+        <br />
+        <p>
+          I started his blog as a way to journal down my thoughts, and also to
+          write about personal finance.
+        </p>
+      </div>
+      <div className="flex flex-col gap-8">
         <BlogPosts />
       </div>
     </section>
-  )
+  );
 }
