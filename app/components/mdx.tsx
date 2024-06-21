2853d53c9c9428e404fa013f1ae662aebc847db1
andlimey <andychan1451@gmail.com>
2024-06-21 21:05:33 +0800

    Add support for tables in mdx using the remarkGfm plugin

--- app/components/mdx.tsx
+++ app/components/mdx.tsx
@@ -3,6 +3,11 @@ import Image from "next/image";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import { highlight } from "sugar-high";
 import React from "react";
+import remarkGfm from "remark-gfm";
+
+const mdxOptions = {
+  remarkPlugins: [remarkGfm],
+};
 
 function Table({ data }) {
   let headers = data.headers.map((header, index) => (
@@ -99,11 +104,12 @@ let components = {
   Table,
 };
 
-export async function CustomMDX(props) {
+export async function CustomMDX({ source }: { source: string }) {
   return (
     <MDXRemote
-      {...props}
-      components={{ ...components, ...(props.components || {}) }}
+      source={source}
+      components={{ ...(components || {}) }}
+      options={{ mdxOptions }}
     />
   );
 }
